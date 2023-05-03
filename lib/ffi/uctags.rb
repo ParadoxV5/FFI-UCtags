f4e5628c6c010b94d3f67b32aa7fec6e712ebbcf
ParadoxV5 <paradox.ver5@gmail.com>
2023-05-03 11:25:48 -0600

    `#initialize`

--- lib/ffi/uctags.rb
+++ lib/ffi/uctags.rb
@@ -1,6 +1,12 @@
 require 'ffi'
 
 class FFI::UCTags
+  def initialize(namespace = FFI)
+    if !namespace.is_a? Module or namespace.is_a? Class
+      raise "wrong argument type #{namespace.class} (expected Module)"
+    end
+    @ns =  (namespace <= FFI) ? namespace : Module.new.include(namespace, FFI)
+  end
   
 end
 
