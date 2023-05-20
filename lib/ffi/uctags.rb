979bb0ca82b1e3e0b0d17a3533c6a73b600e7938
ParadoxV5 <paradox.ver5@gmail.com>
2023-05-20 12:14:38 -0600

    Flatten `Builder` to instance level
    cleanup pending

--- lib/ffi/uctags.rb
+++ lib/ffi/uctags.rb
@@ -16,7 +16,6 @@ end
 
 require 'ffi'
 require_relative 'uctags/version'
-require_relative 'uctags/builder'
 
 # Auto-load FFI functions and etc. by parsing a C header file. See [the README](..) for an overview of the gem.
 # 
@@ -31,7 +30,7 @@ class FFI::UCTags
     # 
     # The customized module does not have to cover all utilized FFI modules/classes –
     # {.call} will fall back to source from FFI for modules/classes not found from this module (see {.ffi_const}).
-    # However, those that the ffi_module do provide must match in layouts and functionalities as those of {FFI}.
+    # However, those that the module do provide must match in layouts and functionalities as those of {FFI}.
     # 
     # @return [Module]
     attr_reader :ffi_module
@@ -68,7 +67,7 @@ class FFI::UCTags
       lib = Module.new
       lib.extend(ffi_const :Library)
       lib.ffi_lib library_name
-      builder = Builder.new(lib)
+      builder = new(lib)
       
       #noinspection SpellCheckingInspection
       cmd = %w[ctags --language-force=C --kinds-C=mpstuxz --fields=NFPkst -nuo -]
@@ -121,4 +120,60 @@ class FFI::UCTags
   end
   # Initialize class variable
   self.ffi_module = FFI
+  
+  ## Indefinite API follows ##
+  
+  def initialize(lib)
+    @lib = lib
+  end
+  
+  #noinspection RubyResolve
+  def typeref(fields)
+    type, name = fields.fetch('typeref').split(':', 2)
+    if 'typename'.eql? type # non-derived type
+      case name
+      when /[*\[]/ # `t *`, `t []`, `t (*) []`, `t (*)(…)`, etc.
+        FFI::TYPE_POINTER
+      when '_Bool'
+        FFI::TYPE_BOOL
+      when 'long double'
+        FFI::TYPE_LONGDOUBLE
+      else
+        # Check multi-keyword integer types (does not match unconventional styles such as `int long untyped long`)
+        # duplicate `t` capture name is intentional
+        if /\A((?<unsigned>un)?signed )?((?<int_type>long|short|long long)( int)?|(?<int_type>int|char))\z/ =~ name
+          int_type.tr!(' ', '_') # namely `long long` -> 'long_long'
+          unsigned ? :"u#{int_type}" : int_type.to_sym
+        else
+          name.to_sym # Fall back to type map
+        end.then do|name_sym|
+          @lib.find_type(name_sym)
+        rescue TypeError
+          # Assume the unknown type is a pointer alias defined in another file.
+          # This should just propagate an exception once multi-file parsing is supported.
+          warn "unrecognized type #{name}, falling back to `:pointer`"
+          FFI::TYPE_POINTER
+        end
+      end
+    else # `struct` or `union` (`enum` not yet supported)
+      @lib.const_get(name).by_value
+    end
+  end
+  
+  def prefix(*prefixes) = @prefix = prefixes
+  def suffix(*suffixes) = @suffix = suffixes
+  def <<(arg) = @args << arg
+  
+  def open(receiver = @lib, method)
+    if @method
+      if @prefix.empty? and @suffix.empty?
+        @receiver.public_send(@method, *@args)
+      else
+        @receiver.public_send(@method, *@prefix, @args, *@suffix)
+      end
+    end
+    @receiver, @method = receiver, method
+    @prefix, @suffix, @args = [], [], []
+  end
+  def close = open nil, nil
 end
