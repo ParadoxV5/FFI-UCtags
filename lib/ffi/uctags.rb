90f2d08c10416a6ed97d4c319f5fefdbff80b464
ParadoxV5 <paradox.ver5@gmail.com>
2023-06-26 23:12:27 -0600

    Use “bundled” u-ctags
    Also revert f037215 – no longer need the pre-check

--- lib/ffi/uctags.rb
+++ lib/ffi/uctags.rb
@@ -1,19 +1,4 @@
 # frozen_string_literal: true
-
-# pre-check – fail fast (raise `LoadError`) if u-ctags not found
-begin
-  IO.popen(%w[ctags --version], err: :err) do|cmd_out|
-    cmd_out = cmd_out.gets
-    if cmd_out
-      puts cmd_out if $VERBOSE
-      break if cmd_out.start_with?('Universal Ctags')
-    end
-    raise LoadError, 'installed `ctags` is not Universal Ctags?'
-  end
-rescue SystemCallError
-  raise LoadError, 'is Universal Ctags installed?'
-end
-
 require 'ffi'
 require_relative 'uctags/version'
 
@@ -88,7 +73,7 @@ class FFI::UCtags
     def call(library_name, header_path, &blk)
       instance = new(library_name)
       #noinspection SpellCheckingInspection this command use letter flags
-      cmd = %w[ctags --language-force=C --param-CPreProcessor._expand=1 --kinds-C=defgmpstuvxz --fields=NFPkSst --fields-C={macrodef} -nuo -] #: Array[_ToS]
+      cmd = %W[#{File.expand_path('../../../u-ctags/bin/ctags', __FILE__)} --language-force=C --param-CPreProcessor._expand=1 --kinds-C=defgmpstuvxz --fields=NFPkSst --fields-C={macrodef} -nuo -]
       cmd.insert(2, '-V') if $DEBUG
       cmd << header_path
       IO.popen(cmd) do|cmd_out|
