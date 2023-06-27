32ed82dfe4f97b45315c1a174de932a9e9c9b3c3
ParadoxV5 <paradox.ver5@gmail.com>
2023-06-26 22:24:15 -0600

    Create `Makefile` (embedded in `extconf.rb`)
    This project has made me learn Makefiles… haha.

--- u-ctags.extconf.rb
+++ u-ctags.extconf.rb
@@ -1,3 +1,4 @@
+# Configure u-ctags
 [
   %w[./autogen.sh
   ],
@@ -12,3 +13,15 @@
     --without-included-regex
   ]
 ].each { system(*_1, chdir: 'u-ctags/src', exception: true) }
+
+# Create Makefile that forwards select `TARGETS` to [u-ctags Makefile](u-ctags/src/Makefile)
+File.write("Makefile", <<EOF)
+TARGETS := all install uninstall clean distclean 
+.PHONY: $(TARGETS) help
+$(TARGETS)::
+\t$(MAKE) -C u-ctags/src $@
+distclean::
+\t-rm -f Makefile
+help:
+\t@echo "Make corresponding u-ctags target (one of: $(TARGETS))"
+EOF
